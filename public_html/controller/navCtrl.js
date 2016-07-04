0273de9f4645e3638fd1532ed412022bcddb5518
techvedika <techvedika@techvedika-PC>
2016-07-04 18:50:11 +0530

    Left navigation completed with dropdown feature

---
+++ public_html/controller/navCtrl.js
@@ -0,0 +1,16 @@
+app.controller('navController', function ($scope, servicecall) {
+
+    $scope.showMessage = "Navigation Page";
+
+    $scope.subnav = 1;
+    $scope.navToggle = 0;
+
+    $scope.navExpand = function (evt, lvl) {
+        if (lvl != 0) {
+            $scope.subnav = lvl;
+            $scope.navToggle = $scope.navToggle == 0 ? 1 : 0;
+        } else {
+            alert("this does not have submenu");
+        }
+    }
+});
