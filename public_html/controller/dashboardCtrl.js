610605d69422c35b5627ad7494a61cfb5776227a
techvedika <giri.endla@techvedika.com>
2016-07-12 18:57:49 +0530

    Node Configurations for Gulp, Grunt, Bower

--- public_html/controller/dashboardCtrl.js
+++ public_html/controller/dashboardCtrl.js
@@ -4,3 +4,11 @@ app.controller('dashboardController', function ($scope, servicecall) {
 
     servicecall.dashboardOnLoad();
 });
+
+(function($scope){
+	'use strict';
+        $scope.varibale = "valeu";
+	var iifi = "iifi in iffi";
+}());
+
+var ifi = "external ifi";
